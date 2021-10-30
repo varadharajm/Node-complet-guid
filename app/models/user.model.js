00a9f7cb8bc67921f051686eaaf7065b9508480c
varadharjm <varadharaj@biovustech.com>
2021-10-30 17:20:26 +0530

    commit with user.model.js chenges

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 const userModel = mongoose.Schema({
+   
     first_name:{type:String, minLength:3, maxLength:15, required:true},
     last_name :{type:String, minLength:3, maxLength:15, required:true},
     dob:{type:Date,required:true,trim: true},
     email:{type:String, required:true, lowercase:true},
     password:{type:String, required:true, minLength:8},
+    //role:{enum:[Admin,Hr,SuperAdmin,Technical, DigitalMarketing, Other]},
     town:{ type:String, required:true, minLength:5, maxLength:50},
     pincode:{ type:Number, required:true, maxLength:6, minLength:6},
     mobile:{ type:Number, required:true, minLength:10, maxLength:10},
@@ -13,3 +15,9 @@ const userModel = mongoose.Schema({
 },{ timestamps: {} });
 
 module.exports= mongoose.model( "user_data", userModel);
+
+// {
+    
+//     employeid:{type: String,required:true}
+//     address:{ type:String,maxLength:50}
+// }
